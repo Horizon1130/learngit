dcbc4f4b25df8f2e2cc6e96a7d3c8154ebce13be
Horizon <1063181803@qq.com>
2023-11-06 15:05:39 +0800

    增加了注释

--- 指针.cpp
+++ 指针.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 int main()
 {
-	int a=10;
+	int a=10;  #定义
 	int *p;
 	p=&a; 
-	cout<<*p<<endl;
-	cout<<*p++<<endl;
+	cout<<*p<<endl;   #输出
+	cout<<*p++<<endl;  #输出
 	return 0;
 } 
